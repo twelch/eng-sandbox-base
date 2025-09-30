99d8c0dd1c18d6f9f55839432877f42c9afd7385
Tim Welch <tim.j.welch@gmail.com>
2025-09-29 22:36:34 -0700

    loosen allowed domains

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        'localhost:3000',
+        '127.0.0.1:3000',
+        '*.app.github.dev', // Allow GitHub Codespaces domains
+        '*.githubpreview.dev', // Allow GitHub Codespaces preview domains
+      ]
+    }
+  }
 };
 
 export default nextConfig;
